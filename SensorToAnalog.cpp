8cefb40cee90bbfe934dd644c4a9150af0f1331d
swetha0123 <swethapraba2310@gmail.com>
2022-03-31 17:37:02 +0530

    Including another file

---
+++ SensorToAnalog.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int max_range;
+    float f;
+    scanf("%u",&max_range);
+    scanf("%f",&f);
+    printf("%f",(f/max_range)*5);
+
+    return 0;
+}
+
+
