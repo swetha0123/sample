ecced46ab2dae1f4afd05a9fd866fde65eb1494b
swetha0123 <swethapraba2310@gmail.com>
2022-03-31 17:33:39 +0530

    set_bit

---
+++ set_bit.cpp
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    unsigned int a;
+    unsigned int b;
+    printf("enter the number");
+    scanf("%u",&a);
+    printf("enter the bit to be checked");
+    scanf("%u",&b);
+    if((a&((int)pow(2,b)))!=0){
+        printf("the bit is set");
+    }
+    else{
+        printf("the bit is not set");
+    }
+    return 0;
+}
+
+
